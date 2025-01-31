22e5f792233a2ba1fba0dadb0f5fca0e9b53b106
PoloCotarelo <polo.cotarelo@gmail.com>
2025-01-31 12:09:56 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+ml.onStart(ml.event.Copa, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+ml.onStart(ml.event.Vertical, function () {
+    basic.showIcon(IconNames.Sword)
+})
+ml.onStart(ml.event.Tejado, function () {
+    basic.showIcon(IconNames.House)
+})
+ml.onStart(ml.event.Nada, function () {
+    basic.showIcon(IconNames.No)
+})
+ml.onStart(ml.event.Horizontal, function () {
+    basic.showIcon(IconNames.Asleep)
+})
